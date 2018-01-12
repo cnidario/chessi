90057a1cd76d5da4d821fed98d6755727c461d65
cnidario <a@a.a>
2018-01-12 15:10:00 +0100

    avance

--- chessi.c
+++ chessi.c
@@ -5,11 +5,27 @@ typedef enum PieceType { Pawn, Rook, Knight, Bishop, Queen, King, UnmovedSpecial
 typedef struct ChessSt {
 	unsigned long long piece_present[COLOR_NB]; //64*2 bits presencia/color bitboard
 	unsigned char pieces[32]; //4bits por pieza
-	unsigned short king_pos; //posición de los reyes, 3x2=6 bits por posición x2 = 12+1=13 bit turno
+	//posición de los reyes, 3x2=6 bits por posición x2 = 12+1=13 bit turno
+	//2*2 = 4 bits posibilidad de enroque (izqda. y dcha)
+	//3b + 1 = 4 indicación en passant + columna
+	//total = 22bits, passant + enroque = 8, aparte
+	unsigned short king_pos;
+	unsigned char castling_passant;
 } ChessSt;
 typedef unsigned short Move;
 
-typedef enum Mask { TURN_BIT = 12, WKING_POSMK = 0x3f, BKING_POSMK = 0xfc0, COLOR_BIT = 3 };
+typedef enum Mask { TURN_BIT = 12, 
+					WKING_POSMK = 0x3f,
+					BKING_POSMK = 0xfc0,
+					COLOR_BIT = 3,
+					CASTLING_BIT = 7,
+					LEFT_CASTLING_BIT = 7,
+					RIGHT_CASTLING_BIT = 6, //5 y 4 para negras
+					PASSANT_BIT = 3,
+					PASSANT_COLUMN_MASK = 0x7,
+					LEFT = 0,
+					RIGHT = 1
+			};
 #define MOVE1_POSMK WKING_POSMK
 #define MOVE2_POSMK BKING_POSMK
 
@@ -19,10 +35,12 @@ typedef enum Mask { TURN_BIT = 12, WKING_POSMK = 0x3f, BKING_POSMK = 0xfc0, COLO
 #define POS2(move) (((move) & MOVE2_POSMK) >> 6)
 #define TURN(game) (FLAG((game)->king_pos, TURN_BIT))
 #define PRESENT(game, pos, color) (FLAG((game)->piece_present[color], pos))
+#define OCUPPIED(game, pos) (PRESENT(game, pos, WHITE) || PRESENT(game, pos, BLACK))
 #define PIECE_AT(game, pos) (((pos)%2) ? \
 								  ((game)->pieces[(pos)/2] & 0xF0) >> 4 \
 								: ((game)->pieces[(pos)/2] & 0xF))
 #define COLOR(game, pos) (PRESENT(game, pos, BLACK))
+#define OPONENT(color) (((color) + 1) % 2)
 #define OOB(p) ((p) >= 64) //out of bounds
 #define COLUMN(pos) ((pos)%8)
 #define ROW(pos) ((pos)/8)
@@ -31,6 +49,10 @@ typedef enum Mask { TURN_BIT = 12, WKING_POSMK = 0x3f, BKING_POSMK = 0xfc0, COLO
 #define REACHABLE(from, to, dir) (((to) - (from)) % (dir) && \
 							((dir) > 0 && (from) < (to)) || \
 							((dir) < 0 && (from) > (to)) )
+#define CANCASTLE_(game, color, side) (FLAG((game)->castling_passant, CASTLING_BIT + (side) + (color)<<1))
+#define CANCASTLE(game, side) (CANCASTLE_(game, TURN(game), side))
+#define PASSANT(game) (FLAG((game)->castling_passant, PASSANT_BIT))
+#define PASSANT_COL ((game)->castling_passant & PASSANT_COLUMN_MASK)
 
 /*
 typedef struct Direction {
@@ -74,9 +96,35 @@ int validMove(ChessSt *game, Move move) {
 	int valid = game->piece_present[CURR_TURN(game)] & (1 << (move & MOVE1_POSMK));
 }
 
+int isValidCastle(ChessSt *game, unsigned char pos1, unsigned char pos2) {
+	unsigned char col, row;
+	int dpos;
+	Color color;
+	int i;
+	col = COLUMN(pos1);
+	row = ROW(pos1);
+	dpos = pos1 - pos2;
+	color = TURN(game);
+	//5.1. movimiento enroque
+	if((col == 4 && (row == color?7:0)) && ABS(dpos) == 2) {				
+		if(dpos > 0) { //5.1.1. enroque largo
+			if(!CANCASTLE(game, LEFT)) return -1;
+			for(i = 1; i < 4; i++) {
+				if(OCUPPIED(game, pos1 - i)) return -1;
+			}
+			for(i = 0; i < 5; i++) {
+				if(isMenaced(game, pos1 - i, OPONENT(color))) return -1;
+			}
+		} else { //5.1.2. enroque corto
+			//XXX aquí me quedé, refactorizar y unir los dos casos
+		}
+	}
+}
+
 int validPieceMove(ChessSt *game, unsigned char pos1, unsigned char pos2, unsigned char piece) {
 	int i, j;
-	unsigned char p, col;
+	unsigned char p, col, row;
+	Color color = TURN(game);
 	col = COLUMN(pos1);
 	switch(piece) {
 		// 1. si es torre, el camino entre posición 1 y posición 2 por alguna de las direcciones {N, E, S, O} debe estar libre de piezas
@@ -103,13 +151,14 @@ int validPieceMove(ChessSt *game, unsigned char pos1, unsigned char pos2, unsign
 			}
 			return 0;
 		case King:
-			//movimiento normal del rey
+			//5. movimiento normal del rey
 			for(i = -1; i <= 1; i++)
 				for(j = -1; j <= 1; j++) {
-					if(!i && !j) continue;
+					if(!i && !j) continue; //no movimiento
 					p = pos1 + j + i<<3;
 					if(pos2 == p && !(ABS(col - COLUMN(p)) > 2 || OOB(p))) return 1;
 				}
+			
 
 	}
 
